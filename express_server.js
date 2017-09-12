d58aa66fb98860fc56c94dd5c339979bb038da40
Kritika Ganapathy <ganapathy.kritika@gmail.com>
2017-09-12 23:29:29 +0000

    functional url shortener

--- express_server.js
+++ express_server.js
@@ -1,8 +1,10 @@
-var express = require("express");
-var app = express();
-var PORT = process.env.PORT || 8080; // default port 8080
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 8080; // default port 8080
+const bodyParser = require("body-parser");
 
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
 
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -22,15 +24,42 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:id", (req, res) => {
   let templateVars = {urls: urlDatabase, shortURL: req.params.id};
   res.render("urls_show", templateVars);
 });
 
-app.get("/hello", (req, res) => {
-  res.end("<html><body>Hello <b>World</b></body></html>\n");
+app.post("/urls/", (req, res) => {
+  console.log(req.body);
+  // res.send("OK");
+  let shortURL = generateRandomString()
+  urlDatabase[shortURL] = req.body.longURL
+  res.redirect("urls/" + shortURL)
+});
+
+app.get("/u/:shortURL", (req, res) => {
+  console.log(req.params)
+    if (urlDatabase[req.params.shortURL]){
+      let longURL = urlDatabase[req.params.shortURL]
+    } else {
+      res.end("<html><body>That's not a valid short URL.</body></html>\n")
+    }
+  res.redirect(longURL);
 });
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
+function generateRandomString () {
+  let string = '';
+  const chars = '0123456789abcdefghijklmnopqrstuvwxyz';
+  while (string.length < 6) {
+    string += chars.charAt(Math.floor(Math.random() * chars.length));
+  }
+  return string;
+}
