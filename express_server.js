3da0f9f2c736ad3a67f73145bcbd8f33d9657638
Kritika Ganapathy <ganapathy.kritika@gmail.com>
2017-09-13 21:04:17 +0000

    endpoint added to server to handle POST from /login

--- express_server.js
+++ express_server.js
@@ -29,7 +29,7 @@ app.get("/urls/new", (req, res) => {
 });
 
 app.post("/urls/:id/edit", (req, res) => {
-  urlDatabase[req.params.id] = req.body.longURL
+  urlDatabase[req.params.id] = req.body.longURL;
   res.redirect("/urls/");
 });
 
@@ -45,20 +45,25 @@ app.get("/urls/:id", (req, res) => {
 
 app.post("/urls/", (req, res) => {
   // console.log(req.body);
-  let shortURL = generateRandomString()
-  urlDatabase[shortURL] = req.body.longURL
-  res.redirect("/urls/" + shortURL)
+  let shortURL = generateRandomString();
+  urlDatabase[shortURL] = req.body.longURL;
+  res.redirect("/urls/" + shortURL);
 });
 
 app.get("/u/:shortURL", (req, res) => {
-  let longURL
+  let longURL;
     if (urlDatabase[req.params.shortURL]){
-      longURL = urlDatabase[req.params.shortURL]
+      longURL = urlDatabase[req.params.shortURL];
     } else {
-      res.end("<html><body>That's not a valid short URL.</body></html>\n")
+      res.end("<html><body>That's not a valid short URL.</body></html>\n");
     }
   res.redirect(longURL);
 });
+
+app.post("/login", (req, res) => {
+  res.cookie(Object.keys(req.body)[0], req.body.username);
+  res.redirect("/urls");
+})
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
